f32b6031845c36fb10e2dcb0e71d023110d612a8
Luka-Bos <luka.bosnjak92@gmail.com>
2023-03-03 11:28:31 +0100

    chang

--- app/src/main/java/com/example/aplikacijica/MainActivity.kt
+++ app/src/main/java/com/example/aplikacijica/MainActivity.kt
@@ -1,57 +1,82 @@
 package com.example.aplikacijica
-
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    var counter = 0
+    lateinit var textViewCounter: TextView
+    lateinit var buttonUp: Button
+    lateinit var buttonDown: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Toast.makeText(applicationContext, "onCreate", Toast.LENGTH_SHORT).show()
+        Log.i("MyLog", "onCreate")
         setContentView(R.layout.activity_main)
     }
+
     override fun onStart() {
         super.onStart()
         Toast.makeText(applicationContext, "onStart", Toast.LENGTH_SHORT).show()
         Log.i("MyLog", "onStart")
+
+        textViewCounter = findViewById<TextView>(R.id.textViewCounter)
+        buttonUp = findViewById<Button>(R.id.buttonUp)
+        buttonDown = findViewById<Button>(R.id.buttonDown)
+
+        buttonUp.setOnClickListener{
+            counter++
+            if(counter==10){
+                counter=0
+                val intent = Intent( this, SuccessActivity::class.java). apply {
+                    putExtra( "name", findViewById<TextView> (R.id.plainTextName).text.toString())
+                }
+                startActivity(intent)
+            }
+            super.findViewById<TextView>(R.id.textViewCounter).text = counter.toString();
+        }
+
+        buttonDown.setOnClickListener {
+            if(counter != 0) {
+                counter--
+                super.findViewById<TextView>(R.id.textViewCounter).text = counter.toString();
+            }
+        }
     }
-    override fun onResume () {
+
+    override fun onResume() {
         super.onResume()
         Toast.makeText(applicationContext, "onResume", Toast.LENGTH_SHORT).show()
         Log.i("MyLog", "onResume")
     }
-    override fun onPause () {
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(applicationContext, "onRestart", Toast.LENGTH_SHORT).show()
+        Log.i("MyLog", "onRestart")
+    }
+
+    override fun onPause() {
         super.onPause()
         Toast.makeText(applicationContext, "onPause", Toast.LENGTH_SHORT).show()
         Log.i("MyLog", "onPause")
     }
-    override fun onRestart  () {
-        super.onRestart ()
-        Toast.makeText(applicationContext, "onRestart", Toast.LENGTH_SHORT).show()
-        Log.i("MyLog", "onRestart")
-    }
-    override fun onDestroy () {
-        super.onDestroy()
-        Toast.makeText(applicationContext, "onDestroy", Toast.LENGTH_SHORT).show()
-        Log.i("MyLog", "onDestroy")
-    }
-    override fun onStop () {
+
+    override fun onStop() {
         super.onStop()
         Toast.makeText(applicationContext, "onStop", Toast.LENGTH_SHORT).show()
         Log.i("MyLog", "onStop")
     }
-    fun setOnClickListenerUp()
-    {
-        var textview: TextView = findViewById(R.id.textViewCounter)
-        var counter = Integer.parseInt(textview.text.toString())
-        counter++
-        textview.text = counter.toString()
-    }
-    fun setOnClickListenerDown()
-    {
-        val textview: TextView = findViewById(R.id.textViewCounter)
-        textview.text = "no text lol"
-    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(applicationContext, "onDestroy", Toast.LENGTH_SHORT).show()
+        Log.i("MyLog", "onDestroy")
+    }
 }
